480596596d861858361d45384da7533d568ecc36
Pikachuxxxx <phani.s2909@icloud.com>
2020-09-11 17:52:49 +0530

    Project Initialised

---
+++ Melancholy Engine/Melancholy Engine/Other Files/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Melancholy Engine
+//
+//  Created by Phani Srikar on 11/09/20.
+//  Copyright © 2020 Phani Srikar. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Melancholy Engine/Melancholy Engine/Other Files/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Melancholy Engine
+//
+//  Created by Phani Srikar on 11/09/20.
+//  Copyright © 2020 Phani Srikar. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
